ab08e8acdb1b7658577c682fc8499a41307a527c
leah <leah.wong4@gmail.com>
2015-05-08 16:31:20 +0800

    word

--- dialog_text.php
+++ dialog_text.php
@@ -1,34 +1,7 @@
 <?php include('header.php') ?>
 
-<style>
-    #big_word{
-        position: absolute;
-        top: 200px;
-        text-align: center;
-        font-size: 100px;
-        display: none;
-        color: red;
-    }
-</style>
 
-<h1 id="big_word">I'm on fire</h1>
+
 <a href="javascript: showWord('Mission Failed')">OPEN</a>
-
-<script>
-
-    // Link to open the dialog
-    function showWord(msg){
-
-        $("#big_word").text(msg);
-
-        $("#big_word").show("fast", function(){
-
-            setTimeout(function(){
-                $("#big_word").hide("slow");
-            }, 10000);
-        });
-    }
-
-</script>
 
 <?php include('footer.php') ?>
